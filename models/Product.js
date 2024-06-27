45357a4ca326fe7ce9958cd79a1c58d6e2c839d3
Nikuzo <41453940+NikuzoKO@users.noreply.github.com>
2024-06-27 17:53:52 -0300

    Modelo actualizado, sin el slugify, con todos los tipos de datos

--- models/Product.js
+++ models/Product.js
@@ -1,1 +1,48 @@
-const { Sequelize, Model, DataTypes } = require("sequelize");
+const { Model, DataTypes } = require("sequelize");
+
+class Product extends Model {
+    static initModel(sequelize) {
+        Product.init(
+            {
+                id: {
+                    type: DataTypes.BIGINT.UNSIGNED,
+                    primaryKey: true,
+                    autoIncrement: true,
+                    allowNull: false,
+                },
+                name: {
+                    type: DataTypes.TINYTEXT,
+                    allowNull: false,
+                },
+                description: {
+                    type: DataTypes.TEXT(1000),
+                    allowNull: false,
+                },
+                image: {
+                    type: DataTypes.TINYTEXT,
+                    allowNull: false,
+                },
+                stock: {
+                    type: DataTypes.TINYINT,
+                    allowNull: false,
+                    defaultValue: 0,
+                },
+                price: {
+                    type: DataTypes.FLOAT(5, 2),
+                    allowNull: false,
+                    defaultValue: 0,
+                },
+                trending: {
+                    type: DataTypes.BOOLEAN,
+                    allowNull: false,
+                    defaultValue: false,
+                },
+                slug: {},
+            },
+            {
+                sequelize,
+                modelName: "product",
+            },
+        );
+    }
+}

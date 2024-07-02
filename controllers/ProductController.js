d4b96cb5139c1b710a4b29edde3f42a3588398a5
denisse2911 <134969268+denisse2911@users.noreply.github.com>
2024-07-02 20:20:18 -0300

    products

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -2,8 +2,10 @@ const { Product } = require("../models");
 
 const ProductController = {
     showList: async (req, res) => {
-        const products = await Product.findAll();
-        return res.json({ Products: products });
+        const limit = 10;
+        const offset = req.body.page ? (req.body.page - 1) * limit : 0;
+        const products = await Product.findAll({ offset: offset, limit: limit });
+        return res.json({ products: products });
     },
 };
 

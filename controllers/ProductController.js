ea48dc88f55a53e529761248c2d6d425a27b035e
Nikuzo <41453940+NikuzoKO@users.noreply.github.com>
2024-07-03 09:42:45 -0300

    Added the functionality to add a custom limit of the products

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -2,7 +2,7 @@ const { Product } = require("../models");
 
 const ProductController = {
     showList: async (req, res) => {
-        const limit = 10;
+        const limit = req.body.limit || 10;
         const offset = req.body.page ? (req.body.page - 1) * limit : 0;
         const products = await Product.findAll({ offset: offset, limit: limit });
         return res.json({ products: products });

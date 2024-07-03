aa0947d332932d688d4f15413025a0f12f798b86
Laura Pérez Betarte <lmperezbetarte@gmail.com>
2024-07-03 11:01:26 -0300

    Límite de products

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -2,8 +2,8 @@ const { Product } = require("../models");
 
 const ProductController = {
     showList: async (req, res) => {
-        const limit = req.body.limit || 10;
-        const offset = req.body.page ? (req.body.page - 1) * limit : 0;
+        const limit = req.query.limit ? Number(req.query.limit) : 10;
+        const offset = req.query.page ? (Number(req.query.page) - 1) * limit : 0;
         const products = await Product.findAll({ offset: offset, limit: limit });
         return res.json({ products: products });
     },
